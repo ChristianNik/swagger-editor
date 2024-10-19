de6f47035860dda484b4fbe0268fd06e71d91e8a
Christian Nikel <git@chnikel.de>
2024-10-19 20:04:21 +0200

    List from data

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,51 @@
 <script setup lang="ts">
 import exampleSwagger from "./assets/example-swagger.json";
 import MenuItem from "./components/MenuItem.vue";
+
+const addObjectKeyToObject = (array: object) =>
+  Object.entries(array).map(([method, data]) => ({
+    method,
+    ...data,
+  }));
+
+const getData = () => {
+  return Object.entries(exampleSwagger.paths).map(
+    ([path, availableMethods]) => {
+      return {
+        path,
+        methods: addObjectKeyToObject(availableMethods),
+      };
+    }
+  );
+};
+
+const data = getData();
 </script>
 
 <template>
   <div class="grid grid-cols-3">
     <nav class="">
-      <h2 class="p-3 font-bold text-xl">PET</h2>
-
-      <ul class="">
-        <MenuItem
-          method="post"
-          path="/pet"
-        />
-        <MenuItem
-          method="put"
-          path="/pet"
-        />
-        <MenuItem
-          method="get"
-          path="/pet/findByStatus"
-        />
-      </ul>
+      <detail v-for="tag in exampleSwagger.tags">
+        <summary class="p-3 font-bold text-xl">
+          {{ tag.name.toUpperCase() }}
+        </summary>
+        <ul class="">
+          <div
+            v-for="item in data.map((d) => ({
+              ...d,
+              methods: d.methods.filter((m) => m.tags.includes(tag.name)),
+            }))"
+          >
+            <MenuItem
+              v-for="method in item.methods"
+              :method="method.method"
+              :path="item.path"
+            />
+          </div>
+        </ul>
+      </detail>
     </nav>
-    <main class="col-span-2">dwa</main>
+    <main class="col-span-2"></main>
   </div>
 </template>
 
