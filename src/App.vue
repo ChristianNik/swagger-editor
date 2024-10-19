504cc8c2debf6ed04659dde96ce54086a4a81355
Christian Nikel <git@chnikel.de>
2024-10-19 19:41:41 +0200

    Add basic nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
+import exampleSwagger from "./assets/example-swagger.json";
+import MenuItem from "./components/MenuItem.vue";
 </script>
 
 <template>
-  App
+  <div class="grid grid-cols-3">
+    <nav class="">
+      <h2 class="p-3 font-bold text-xl">PET</h2>
+
+      <ul class="">
+        <MenuItem
+          method="post"
+          path="/pet"
+        />
+        <MenuItem
+          method="put"
+          path="/pet"
+        />
+        <MenuItem
+          method="get"
+          path="/pet/findByStatus"
+        />
+      </ul>
+    </nav>
+    <main class="col-span-2">dwa</main>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  method: "get" | "post" | "put" | "delete";
+  path: string;
+}>();
+</script>
+
+<template>
+  <li class="px-3 py-1 grid hover:bg-gray-50" style="grid-template-columns: 1fr 4fr;">
+    <b
+      :class="{
+        'text-green-500': method.toLowerCase() === 'post',
+        'text-yellow-500': method.toLowerCase() === 'put',
+        'text-blue-500': method.toLowerCase() === 'get',
+        'text-red-500': method.toLowerCase() === 'delete',
+      }"
+    >
+      {{ method.toUpperCase() }}
+    </b>
+    {{ path }}
+  </li>
+</template>
+
+<style scoped></style>
