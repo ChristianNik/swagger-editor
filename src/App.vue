306adab627fe84ff1aae3bd6dd7623636636747c
Christian Nikel <git@chnikel.de>
2024-10-19 20:55:59 +0200

    Render basic responses

--- src/App.vue
+++ src/App.vue
@@ -44,11 +44,70 @@ const selectedItem = swaggerDocument.getMethodForPath("/pet", "put");
         <label>
           Description
           <textarea
-          class="h-32"
+            class="h-32"
             type="text"
             :value="selectedItem.description"
           />
         </label>
+      </div>
+      <div class="p-3 space-y-3 border-t">
+        <label>
+          Summary
+          <input
+            type="text"
+            :value="selectedItem.summary"
+          />
+        </label>
+
+        <!-- <h3 class="flex justify-between items-center">
+          <b>RequestBody</b>
+
+          <select>
+            <option value="">dwa</option>
+          </select>
+        </h3> -->
+
+        <h3 class="flex justify-between items-center">
+          <b>Responses</b>
+        </h3>
+        <div class="relative overflow-x-auto">
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-3"
+                >
+                  Code
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3"
+                >
+                  Description
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                class="bg-white border-b"
+                v-for="response in Object.entries(selectedItem.responses).map(([code, value]) => {
+            (value as any).code = code;
+            return value;
+          })"
+              >
+                <td class="px-6 py-4">{{ (response as any).code }}</td>
+                <td class="px-6 py-4">
+                  {{ (response as any).description }}
+
+                  <div>
+                    {{ (response as any).content }}
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       <pre>
         {{ selectedItem }}
