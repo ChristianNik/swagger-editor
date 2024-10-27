9d78f18f3663cd54b0912b347f30b213dd7c34b1
Christian Nikel <git@chnikel.de>
2024-10-27 20:29:54 +0100

    Add parameters table

---
+++ src/components/ParametersTable.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+defineProps<{
+  data: {
+    name: string;
+    in: "path" | "query";
+    description?: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+    <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+      <tr>
+        <th
+          scope="col"
+          class="px-6 py-3"
+        >
+          Name
+        </th>
+        <th
+          scope="col"
+          class="px-6 py-3"
+        >
+          Location
+        </th>
+        <th
+          scope="col"
+          class="px-6 py-3"
+        >
+          Description
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        class="bg-white border-b"
+        v-for="parameter in data"
+      >
+        <td class="px-6 py-4">
+          {{ parameter.name }}
+        </td>
+        <td class="px-6 py-4">
+          {{ parameter.in }}
+        </td>
+        <td class="px-6 py-4">
+          {{ parameter.description }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
